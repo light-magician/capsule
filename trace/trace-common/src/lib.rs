2e187285b4469d90b6cfc6b56a58527c4980b30e
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2025-08-14 14:22:39 -0700

    feat: prcess syscall struct and check for supported process syscall on x86_64 (AMD64 instruction set)

--- trace/trace-common/src/lib.rs
+++ trace/trace-common/src/lib.rs
@@ -15,3 +15,38 @@ pub struct Event {
     pub arg1: u64,
     pub arg2: u64,
 }
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct ProcEvent {
+    // kernel monotonic timestamp
+    pub ts_ns: u64,
+    // task (thread) ID and thread group id
+    pub pid: u32,
+    pub tid: u32,
+    // syscall number (x86_64). For sched_exit this will be 0
+    pub sysno: u32,
+    // 0 = sys_enter, 1 = sys_exit, 2 = sched_process_exit
+    pub phase: u8,
+    pub _pad: [u8; 3],
+}
+
+/// scope modes (shared so userspace and kernelspace agree)
+pub const MODE_ALL: u32 = 0;
+pub const MODE_CGROUP: u32 = 0;
+
+// Minimal process-syscall set for x86_64
+#[inline(always)]
+pub const fn is_process_syscall_x86_64(n: u32) -> bool {
+    match n {
+        56 | // clone 
+        57 | // fork
+        58 | // vfork
+        59 | // execve
+        322 | // execveat
+        60 | // exit 
+        231  // exit_group
+        => true,
+        _ => false,
+    }
+}
